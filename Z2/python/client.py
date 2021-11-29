98bb9d1fc763502f6cea44d9c58811f90a97ba76
Paweł Müller <pawelmuller42@gmail.com>
2021-11-29 01:11:27 +0100

    Some improvements. Does not work properly.

--- Z2/python/client.py
+++ Z2/python/client.py
@@ -4,6 +4,11 @@ import socket
 class Client:
     def __init__(self):
         self.protocol = "IPv4"
+        self.server = None
+        self.client_socket = None
+        self.prepare()
+
+    def prepare(self):
         self.server = ("127.0.0.1", 8888)
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect(self.server)
@@ -12,7 +17,7 @@ class Client:
         size = len(message)
         try:
             self.client_socket.sendall(message)
-            response = self.client_socket.recv(1024)
+            response = self.client_socket.recv(BUFFER_SIZE)
             return True
         except OSError:
             return False
@@ -27,14 +32,24 @@ class Client:
 class ClientV6(Client):
     def __init__(self):
         super().__init__()
-        self.server = ("::1", 8888)
-        self.client_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
         self.protocol = "IPv6"
+
+    def prepare(self):
+        self.server = ("::1", 8889)
+        self.client_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        self.client_socket.connect(self.server)
 
 
 def main():
-    pass
+    # c = Client()
+    # c.send_message(b"XDv4")
+
+    c = ClientV6()
+    c.send_message(b"XDv6")
 
 
 if __name__ == '__main__':
+    PORT = 8888
+    BUFFER_SIZE = 102400  # >65507
+
     main()
--- Z2/python/server.py
+++ Z2/python/server.py
@@ -5,38 +5,49 @@ import threading
 class Server:
     def __init__(self):
         self.protocol = "IPv4"
+        self.server_properties = None
+        self.server_socket = None
+        self.prepare()
+
+    def prepare(self):
         self.server_properties = ("127.0.0.1", 8888)
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind(self.server_properties)
+        self.server_socket.listen(5)
 
     def listen(self):
         while True:
             connection, address = self.server_socket.accept()
             with connection:
-                print(f"Connection from: {address}", end='\t')
+                print(f"Connection from: {address}", end='\t', flush=True)
                 while True:
                     message = connection.recv(BUFFER_SIZE)
                     if not message:
                         break
                     self.print_message(address, message)
                     connection.sendall(message)
-            print("Connection closed by client.")
+            print("Connection closed by client.", flush=True)
 
     @staticmethod
     def print_message(address, message):
-        print(f"Received packet from {address[0]}:{address[1]}")
-        print(f"Data: {message.decode('utf-8')}")
-        print(f"Data size: {len(message)}")
+        print(f"Received packet from {address[0]}:{address[1]}", flush=True)
+        print(f"Data: {message.decode('utf-8')}", flush=True)
+        print(f"Data size: {len(message)}", flush=True)
 
 
 class ServerV6(Server):
     def __init__(self):
         super().__init__()
         self.protocol = "IPv6"
-        self.server_properties = ("127.0.0.1", 8888)
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def prepare(self):
+        self.server_properties = ("::1", 8889)
+        self.server_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind(self.server_properties)
+        self.server_socket.listen(5)
 
 
 if __name__ == '__main__':
