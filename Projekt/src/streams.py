f403ea99a752685435207ccf06f64220ac98721b
Jakub Robaczewski <kubarob13@gmail.com>
2021-12-20 14:41:37 +0100

    Changed Stream implementation to proper queue

--- Projekt/src/streams.py
+++ Projekt/src/streams.py
@@ -1,19 +1,20 @@
+from queue import Queue
+
+
 class Stream:
     def __init__(self, packet_size=400):
         self._message_size = packet_size
-        self._message_idx = -1
-        self._messages = []
+        self._messages = Queue()
 
     def get_next_message(self):
-        self._message_idx += 1
-        if self._message_idx < len(self._messages):
-            return self._messages[self._message_idx]
-        else:
+        if self._messages.empty():
             return None
+        else:
+            return self._messages.get()
 
-    def add_messages(self, data: bytes, segment_size: int):
+    def fill_queue(self, data: bytes, segment_size: int):
         for i in range(0, len(data), segment_size):
-            self._messages.append(data[i:i + segment_size])
+            self._messages.put(data[i:i + segment_size])
 
 
 class File(Stream):
@@ -22,7 +23,7 @@ class File(Stream):
         with open(filename, 'r', encoding="utf-8") as file:
             self._data = file.read()
             binary_data = bytes(self._data, encoding="utf-8")
-            self.add_messages(binary_data, packet_size)
+            self.fill_queue(binary_data, packet_size)
 
     def get_data(self):
         return self._data
