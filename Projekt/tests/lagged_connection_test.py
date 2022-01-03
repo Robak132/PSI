2f883d04928d11ac810bb51a7a743c70a0642ea0
Jakub Robaczewski <kubarob13@gmail.com>
2022-01-03 15:35:03 +0100

    More fancy tests

---
+++ Projekt/tests/lagged_connection_test.py
@@ -0,0 +1,25 @@
+import logging
+
+from streams import File
+from server import Server
+from client import ClientV4WithLag
+
+
+class TestLaggedConnection:
+    def test_lagged_connection_ipv4(self):
+        server = Server(logging_level=logging.CRITICAL)
+        server.register_stream(1, File("tests/resources/test_file.txt"))
+
+        _, receive_port, *_ = server.ipv4_receive_address
+
+        server.start()
+
+        client = ClientV4WithLag(logging_level=logging.CRITICAL, lag=0.5)
+        data = client.request(1, ("127.0.0.1", receive_port))
+
+        # Data send successfully
+        assert "1234567890\ntest\nzażółć gęsią jaźń" == data.decode("utf-8")
+
+        # Timeout was adjusted for client lag
+        assert server.threads[0].SERVER_ACK_TIMEOUT == 5.12
+        server.stop()
